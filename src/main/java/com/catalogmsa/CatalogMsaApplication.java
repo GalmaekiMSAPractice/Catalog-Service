9777ed100f7b36041c7e7d5e450dfdb9aec78b74
Galmaeki <kjh09800@gmail.com>
2024-12-18 16:25:14 +0900

    초기화 커밋

---
+++ src/main/java/com/catalogmsa/CatalogMsaApplication.java
@@ -0,0 +1,13 @@
+package com.catalogmsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogMsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogMsaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/catalogmsa/CatalogMsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catalogmsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogMsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
